3677d55066f609713fb4db981a79e47f165ff39b
HosseinFeyzbakhshofficial <hossein.feyzbakhshofficial@gmail.com>
2026-07-08 16:47:22 +0200

    test: support dynamic naming in setup_logger to fix utility tests

--- src/utils/logger.py
+++ src/utils/logger.py
@@ -1,6 +1,8 @@
 import logging
 
-
-def setup_logger(level=logging.INFO):
+def setup_logger(name: str = __name__, level=logging.INFO):
+    """
+    Initializes and returns a logger instance with the given name.
+    """
     logging.basicConfig(level=level, format="%(asctime)s - %(levelname)s - %(message)s")
-    return logging.getLogger(__name__)
+    return logging.getLogger(name)
